f8a4c130f09b6eac31298fee8d1798a4fc5fefaa
Muskan377750 <nov6947sk@gmail.com>
2026-04-27 20:34:40 +0530

    modified validations

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.engine("ejs", ejsMate);
@@ -43,6 +44,16 @@ async function main() {
 // res.send("successful testing");
 // });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 // Index Route
 app.get(
   "/listings",
@@ -58,31 +69,42 @@ app.get("/listings/new", (req, res) => {
 });
 
 // Create Route
+// app.post(
+//   "/listings",
+//   wrapAsync(async (req, res, next) => {
+// if (!req.body.listing.image) {
+//   req.body.listing.image = undefined;
+// }
+// if (!req.body.listing) {
+//   throw new ExpressError(400, "Send valid data for listing!");
+// }
+// const newListing = new Listing(req.body.listing);
+// if(!newListing.title){
+//     throw new ExpressError(400, "Title is missing!");
+// }
+// if(!newListing.description){
+//     throw new ExpressError(400, "Description is missing!");
+// }
+// if(!newListing.price){
+//     throw new ExpressError(400, "Price is missing!");
+// }
+// if(!newListing.country){
+//     throw new ExpressError(400, "Country is missing!");
+// }
+// if(!newListing.location){
+//     throw new ExpressError(400, "Location is missing!");
+// }
+//     await newListing.save();
+//     res.redirect("/listings");
+//   }),
+// );
+
+// Create new Route
 app.post(
   "/listings",
+  validateListing,
   wrapAsync(async (req, res, next) => {
-    // if (!req.body.listing.image) {
-    //   req.body.listing.image = undefined;
-    // }
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Send valid data for listing!");
-    }
     const newListing = new Listing(req.body.listing);
-    if(!newListing.title){
-        throw new ExpressError(400, "Title is missing!");
-    }
-    if(!newListing.description){
-        throw new ExpressError(400, "Description is missing!");
-    }
-    if(!newListing.price){
-        throw new ExpressError(400, "Price is missing!");
-    }
-    if(!newListing.country){
-        throw new ExpressError(400, "Country is missing!");
-    }
-    if(!newListing.location){
-        throw new ExpressError(400, "Location is missing!");
-    }
     await newListing.save();
     res.redirect("/listings");
   }),
@@ -111,6 +133,7 @@ app.get(
 // Update Route
 app.put(
   "/listings/:id",
+  validateListing,
   wrapAsync(async (req, res) => {
     if (!req.body.listing) {
       throw new ExpressError(400, "Send valid data for listing!");
