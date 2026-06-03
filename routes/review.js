102c7b706d94a199dacb9a6e43003b717229f085
Muskan377750 <nov6947sk@gmail.com>
2026-06-03 20:00:42 +0530

    implemented flash

--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const {reviewSchema} = require("../schema.js");
+const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 
 const validateReview = (req, res, next) => {
@@ -17,21 +17,30 @@ const validateReview = (req, res, next) => {
 };
 
 //post route for reviews
-router.post("/",validateReview,wrapAsync(async(req,res)=>{
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-  listing.reviews.push(newReview);
-  await newReview.save();
-  await listing.save();
-  res.redirect(`/listings/${listing._id}`);
-}));
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    req.flash("success", "New review created!");
+    res.redirect(`/listings/${listing._id}`);
+  }),
+);
 
 // delete route for reviews
-router.delete("/:reviewId",wrapAsync(async(req,res)=>{
-  let {id,reviewId} = req.params;
-  await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/listings/${id}`);
-}));
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    req.flash("success", "Review deleted!");
+    res.redirect(`/listings/${id}`);
+  }),
+);
 
 module.exports = router;
