7acd8b586784de4fc377806589148f375dbffb63
Muskan377750 <nov6947sk@gmail.com>
2026-06-10 19:50:32 +0530

    Added passport

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const session = require("express-session");
 const listings = require("./routes/listing.js");
 const reviews  = require("./routes/review.js");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 main()
   .then(() => {
@@ -74,6 +77,14 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
