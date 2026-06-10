e78dd3e9602ac91fc374c88f45db3caf81905b48
Muskan377750 <nov6947sk@gmail.com>
2026-06-10 22:17:25 +0530

    Added signup page

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema , reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 const session = require("express-session");
-const listings = require("./routes/listing.js");
-const reviews  = require("./routes/review.js");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter  = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 main()
   .then(() => {
@@ -92,8 +94,18 @@ app.use((req,res,next)=>{
   next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews",reviews);
+app.get("/demouser",async(req,res)=>{
+  let fakeUser = new User({
+    email: "Student@gmail.com",
+    username: "Delta-student",
+  });
+  let registeredUser = await User.register(fakeUser,"HelloWorld");
+  res.send(registeredUser);
+})
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 
 // app.all("/*", (req, res, next) => {
