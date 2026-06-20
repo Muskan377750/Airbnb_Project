8993b9cc8c1c587fe1f84549e1f81093af8778cf
Muskan377750 <nov6947sk@gmail.com>
2026-06-20 18:31:18 +0530

    Added logout

--- routes/user.js
+++ routes/user.js
@@ -31,5 +31,14 @@ router.post("/login",passport.authenticate("local",{failureRedirect:"/login",fai
     res.redirect("/listings");
 });
 
+router.get("/logout",(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","You are logged out!");
+        res.redirect("/listings");
+    });
+});
 
 module.exports = router;
