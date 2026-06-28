6d9bc3f054593dc8179361149a0100a2dc5d74ab
Muskan377750 <nov6947sk@gmail.com>
2026-06-28 21:18:41 +0530

    authentication

--- routes/user.js
+++ routes/user.js
@@ -3,47 +3,59 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const User = require("../models/user");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
 });
 
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try{
-    let {username,email,password} = req.body;
-    const newUser = new User({username,email});
-    const registeredUser = await User.register(newUser,password);
-    console.log(registeredUser);
-    req.login(registeredUser,(err)=>{
-        if(err){
-            return next(err);
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
         }
-         req.flash("success","Welcome to Wanderlust:)");
-         res.redirect("/listings");
-    })
-    } catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");
-    }
-}));
-
-router.get("/login", (req,res)=>{
-    res.render("users/login.ejs");
-});
-
-router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async(req,res)=>{
-    req.flash("success","Wecome back to Wanderlust!");
-    res.redirect("/listings");
-});
-
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","You are logged out!");
+        req.flash("success", "Welcome to Wanderlust:)");
         res.redirect("/listings");
-    });
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  }),
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",saveRedirectUrl,
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    let redirectUrl = res.locals.redirectUrl||"/listings";
+    req.flash("success", "Wecome back to Wanderlust!");
+    res.redirect(redirectUrl);
+  },
+);
+
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You are logged out!");
+    res.redirect("/listings");
+  });
 });
 
 module.exports = router;
