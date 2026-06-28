8a08457725cb916fd676f2e4ba7283429cbf4fd0
Muskan377750 <nov6947sk@gmail.com>
2026-06-28 21:17:25 +0530

    listing modification

--- models/listing.js
+++ models/listing.js
@@ -1,42 +1,47 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Review = require("./review.js"); 
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
-    title:{
-        type:String,
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) => (v === "" ? undefined : v),
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-    description:{
-        type:String,
-    },
-    image:{
-        type:String,
-        default:"https://images.unsplash.com/photo-1588880331179-bc9b93a8cb5e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? undefined : v
-    },
-    price:{
-        type:Number,
-    },
-    location:{
-        type:String,
-    },
-    country:{
-        type:String,
-    },
-    reviews : [
-        {
-            type : Schema.Types.ObjectId,
-            ref: "Review",
-        },
-    ]
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-listingSchema.post("findOneAndDelete",async(listing)=>{
-    if(listing){
-        await Review.deleteMany({_id: {$in:listing.reviews}});
-    }
-})
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 
 module.exports = Listing;
